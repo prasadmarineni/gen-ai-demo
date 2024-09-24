02878f442c8a7e01b14557d58a7009148377fdf8
Prasad Marineni <prasadmarineni@gmail.com>
2024-09-24 23:10:39 +0530

    Resources Path Setup

--- src/main/java/com/genai/app/gen_ai_demo/controller/FileUploadController.java
+++ src/main/java/com/genai/app/gen_ai_demo/controller/FileUploadController.java
@@ -1,10 +1,15 @@
 package com.genai.app.gen_ai_demo.controller;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,7 +30,45 @@ public class FileUploadController {
 	@Autowired
 	private FileService fileService;
 
-	private final String uploadDir = "/Users/prasadmarineni/Documents/workspace/gen-ai/gen-ai-demo/files/";
+	@Autowired
+    private ResourceLoader resourceLoader;
+
+	private static String uploadDir = null;;
+	
+    public String getResourcesPath() {
+        Resource resource = resourceLoader.getResource("classpath:application.properties");
+        String resourcePath;
+		try {
+			resourcePath = resource.getFile().getAbsolutePath();
+			Path absolutePath = Paths.get(resourcePath).toAbsolutePath();
+
+	        // Navigate back two directories to get to the root of the project
+	        Path projectRootPath = absolutePath
+	                .getParent() // classes directory
+	                .getParent() // target directory
+	                .getParent(); // project root directory
+
+	        // Add src/main/resources to the path
+	        Path resourcesPath = projectRootPath.resolve("src/main/resources/files/");
+
+	        System.out.println("Absolute Path to resources: " + resourcesPath);
+	        
+	        return resourcesPath.toString();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return "./";
+    }
+    
+    
+	public String getUploadDir() {
+		if(uploadDir == null)
+			uploadDir = getResourcesPath() + File.separator; 
+		return uploadDir;
+	}
+
 
 	@GetMapping("/files")
 	public List<String> listUploadedFiles() {
@@ -49,10 +92,10 @@ public class FileUploadController {
 
 		try {
 			// Save the uploaded file or process it directly
-			String filePath = uploadDir + fileName;
+			String filePath = getUploadDir() + fileName;
 
 			// Create the directory if it doesn't exist
-			File dir = new File(uploadDir);
+			File dir = new File(getUploadDir());
 			if (!dir.exists()) {
 				dir.mkdirs(); // This will create the directory and any necessary parent directories
 			}
